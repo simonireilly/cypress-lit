e9b8af3f9743f6f2f1d363bfbb625882d269460f
simonireilly <simonireilly@gmail.com>
2023-07-26 19:08:33 +0100

    chore: compile types to provide inline documentation

--- types/mount.d.ts
+++ types/mount.d.ts
@@ -10,5 +10,9 @@ interface MountOptions<T extends HTMLElement> extends Partial<StyleOptions> {
     log?: boolean;
     properties?: Partial<T>;
 }
+/**
+ * Mount places a web component into the `id="data-cy-root"` elements
+ * in component testing
+ */
 export declare const mount: <T extends keyof HTMLElementTagNameMap>(template: TemplateResult | string, options?: MountOptions<HTMLElementTagNameMap[T]>) => Cypress.Chainable<JQuery<HTMLElementTagNameMap[T]>>;
 export {};
